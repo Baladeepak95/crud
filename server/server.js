53c0532bd65a40d3d1aa25fb9ba6ec5b0ed28cb0
Baladeepak95 <bala@enunui.com>
2018-06-06 04:13:19 +0530

    CRUD added

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const _ = require('lodash');
+const express = require('express');
+const { ObjectID } = require ('mongodb');
+const bodyParser = require('body-parser');   
+
+
+// const users = require ('');   
+require('../db');
+require('../routes/routes.js');
+
+
+
